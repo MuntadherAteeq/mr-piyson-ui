ec27c073c5477e3c3ebb6201d7c8eac43e472d71
MuntadherAteeq <mun.ateeq@outlook.com>
2024-02-29 16:03:02 +0300

    Update package name and homepage URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Link, Outlet } from "react-router-dom";
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import { Link, Outlet } from "react-router-dom"
 
 function App() {
   return (
@@ -17,16 +17,18 @@ function App() {
       <h1>Vite + React</h1>
 
       <nav>
-        <Link to="/vite-react-router/">Home</Link>
+        <Link to="/mr-piyson-UI/">Home</Link>
         {" | "}
-        <Link to="/vite-react-router/contact">Contact</Link>
+        <Link to="/mr-piyson-UI/contact">Contact</Link>
       </nav>
 
       <Outlet />
 
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Home } from "./pages/Home.tsx";
-import { Contact } from "./pages/Contact.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { Home } from "./pages/Home.tsx"
+import { Contact } from "./pages/Contact.tsx"
 
 const router = createBrowserRouter([
   {
-    path: "/vite-react-router/",
+    path: "/mr-piyson-UI/",
     element: <App />,
     children: [
       {
-        path: "/vite-react-router/",
+        path: "/mr-piyson-UI/",
         element: <Home />,
       },
       {
-        path: "/vite-react-router/contact",
+        path: "/mr-piyson-UI/contact",
         element: <Contact />,
       },
     ],
   },
-]);
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
